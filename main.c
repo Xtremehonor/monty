dfc03ce1dc8d716872eebcbecddab60b3f48ad0b
Getayawkal W <104856930+Xtremehonor@users.noreply.github.com>
2023-10-20 01:57:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -14,50 +14,50 @@ MontyContext_t MontyContext = {NULL, NULL, NULL, 0};
  */
 int main(int argc, char *argv[])
 {
-    char *line_content;
-    FILE *monty_file;
-    size_t buffer_size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	char *line_content;
+	FILE *monty_file;
+	size_t buffer_size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    monty_file = fopen(argv[1], "r");
-    MontyContext.file = monty_file;
+	monty_file = fopen(argv[1], "r");
+	MontyContext.file = monty_file;
 
-    if (!monty_file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (!monty_file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while (read_line > 0)
-    {
-        for (size_t i = 0; i < buffer_size; i++)
-        {
-            line_content[i] = '\0'; /* Initialize buffer */
-        }
+	while (read_line > 0)
+	{
+		for (size_t i = 0; i < buffer_size; i++)
+		{
+			line_content[i] = '\0'; /* Initialize buffer */
+		}
 
-        read_line = getline(&line_content, &buffer_size, monty_file);
-        MontyContext.content = line_content;
-        counter++;
+		read_line = getline(&line_content, &buffer_size, monty_file);
+		MontyContext.content = line_content;
+		counter++;
 
-        if (read_line > 0)
-        {
-            execute(line_content, &stack, counter, monty_file);
-        }
-    }
+		if (read_line > 0)
+		{
+			execute(line_content, &stack, counter, monty_file);
+		}
+	}
 
-    free(line_content);
-    clear_stack(stack);
-    fclose(monty_file);
-    return (0);
-}
+	free(line_content);
+	clear_stack(stack);
+	fclose(monty_file);
+	return (0);
+	}
 
 void do_nothing(void)
 {
