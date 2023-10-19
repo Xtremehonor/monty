18d709fa3e143b087aa814f0a43080782d1bbecd
Getayawkal W <104856930+Xtremehonor@users.noreply.github.com>
2023-10-20 02:12:04 +0300

    update pstr

--- pstr.c
+++ pstr.c
@@ -1,25 +1,26 @@
 #include "monty.h"
 /**
- * pstr - prints the string starting at the top of the stack,
- * followed by a new
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * pstr - Prints the string starting at the
+ * top of the stack, followed by a new line.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number.
+ * Return: No return.
+ */
 void pstr(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	(void)counter;
+stack_t *current_node;
 
-	h = *head;
-	while (h)
+(void)counter;
+current_node = *head;
+
+while (current_node)
+{
+	if (current_node->n > 127 || current_node->n <= 0)
 	{
-		if (h->n > 127 || h->n <= 0)
-		{
-			break;
-		}
-		printf("%c", h->n);
-		h = h->next;
+		break;
 	}
-	printf("\n");
+	printf("%c", current_node->n);
+	current_node = current_node->next;
+}
+printf("\n");
 }
