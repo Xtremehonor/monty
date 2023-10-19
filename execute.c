d0d211e1de1599a7930995e1ad9e3641bbda1b60
Getayawkal W <104856930+Xtremehonor@users.noreply.github.com>
2023-10-20 00:05:35 +0300

    updated main.c

--- execute.c
+++ execute.c
@@ -10,12 +10,23 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", push}, {"pall", pall}, {"pint", pint },
-				{"pop", pop}, {"swap", swap}, {"add", add},
-				{"nop", f_nop}, {"sub", f_sub}, {"div", f_div},
-				{"mul", f_mul}, {"mod", f_mod}, {"pchar", f_pchar},
-				{"pstr", f_pstr}, {"rotl", f_rotl}, {"rotr", f_rotr},
-				{"queue", f_queue}, {"stack", f_stack},
+				{"push", push},
+				{"pall", pall},
+				{"pint", pint },
+				{"pop", pop},
+				{"swap", swap},
+				{"add", add},
+				{"nop", nop},
+				{"sub", sub},
+				{"div", div},
+				{"mul", mul},
+				{"mod", mod},
+				{"pchar", pchar},
+				{"pstr", pstr},
+				{"rotl", rotl},
+				{"rotr", rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
@@ -41,3 +52,4 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	exit(EXIT_FAILURE); }
 	return (1);
 }
+
