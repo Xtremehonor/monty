a085081bf9635e430e6990a243d5dc04f9dcb7d7
Getayawkal W <104856930+Xtremehonor@users.noreply.github.com>
2023-10-19 18:31:47 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -13,7 +13,6 @@
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -23,11 +22,11 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -36,6 +35,7 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 /**
  * MontyContext_s Structure - Contains variables: arg, file,
  * line content, and stack/queue flag.
